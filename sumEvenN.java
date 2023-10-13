d204474e9a41e6ff6ef0f1725d7a3feb6ec4b7b9
Anu-Shulammite-01 <anu.s@marketsimplified.com>
2023-10-13 13:59:10 +0530

    Sum of Even numbers upto N

---
+++ sumEvenN.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class sumEvenN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("The sum of all even numbers from 1 to " + num + " is " + sum);
+    }
+}
