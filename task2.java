13e82de652e75eb354ff3afd73a89254e4a5fe31
Anu-Shulammite-01 <anu.s@marketsimplified.com>
2023-10-13 13:54:52 +0530

    Task 2

---
+++ task2.java
@@ -0,0 +1,8 @@
+class task2 {
+    public static void main(String[] args) {
+        int int_value = 10;
+        double double_value = 10.5;
+        double double_value2 = int_value;
+        System.out.println(int_value + double_value2);
+    }
+}
