17a4d228395847ae9806469b839c76df685221bf
Anu-Shulammite-01 <anu.s@marketsimplified.com>
2023-10-26 19:12:28 +0530

    Average of array elements

--- avg_Array.java
+++ avg_Array.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class avg_Array {
     public static void main(String args[]) {
-        int arr[] = new int[5];
+        int numArray[] = new int[5];
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < numArray.length; i++) {
+            numArray[i] = sc.nextInt();
         }
         int sum = 0, avg;
-        for (int i : arr) {
+        for (int i : numArray) {
             sum += i;
         }
-        avg = sum / arr.length;
+        avg = sum / numArray.length;
         System.out.println("Average of array elements is " + avg);
     }
 }
